813387b95b4fcb3cd4954a5f3c98681aa0a10198
root <y.m.lima@hotmail.com>
2022-01-22 14:05:16 -0300

    Feature - Added Portugues Language

---
+++ language/portuguese_br/custom_email_and_sms_notifications_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+# Version 1.1.1
+$lang['sms_title']  = 'Enviar Uatizapp/Email/SMS';
+$lang['sms_select_title']  = '* Envie essa notificacao para todos os contatos de';
+$lang['write_your_notification']  = 'Escreva sua mensagem Uatiz/Email/SMS aqui:';
+$lang['sms_textarea_placeholder'] = 'HTML accepted for emails only';
+$lang['attachment_note']  = 'Incluir um anexo (opicional, aplicavel apenas para uatiz/emails):';
+$lang['notification_type']  = 'Selecione o tipo de notificacao:';
+$lang['send'] = 'enviar noticicacao';
+$lang['email_sms'] = 'Enviar Uatiz/Email/SMS';
+$lang['add_edit_templates'] = 'Adicionar / Editar Template';
+$lang['add_templates'] = 'Adicionar Template';
+$lang['edit_templates'] = 'Editar Template';
+$lang['templates'] = 'Templates';
+$lang['template_name'] = 'Nome';
+$lang['template_content'] = 'Conteudo';	
+$lang['send_as_email'] = 'Notificacao por email';
+$lang['send_as_sms'] = 'Notificacao por sms';
+$lang['send_as_uatiz'] = 'Notificacao por uatiz';
+$lang['template_select_title'] = 'Carregar conteudo do template (opicional):';
+
+$lang['customer_or_leads'] = 'Clientes, Leads ou Grupos?';
+$lang['select_customer'] = 'Selecione o cliente';
+$lang['select_lead'] = 'Selecione os Leads';
+$lang['select_group'] = 'Selecione o Grupo';
